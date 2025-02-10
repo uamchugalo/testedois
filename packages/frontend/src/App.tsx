f9f81e8ee96edb0561809eeaabe7d814b64e368b
uamchugalo <fradebraga@gmail.com>
2025-02-10 13:02:36 -0300

    Add React app structure

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="px-4 py-6 sm:px-0">
+          <h1 className="text-2xl font-bold text-gray-900">
+            Welcome to OS FB
+          </h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
